304e4969f0840ebaa0cf9100b987398dbc042903
yamayama62 <gtxkh393@yahoo.co.jp>
2022-07-18 16:24:53 +0900

    4回目のコミット

---
+++ P_factorization.ps1
@@ -0,0 +1,47 @@
+﻿using namespace Microsoft.VisualBasic
+Set-PSDebug -Strict
+Add-Type -Assembly Microsoft.VisualBasic
+
+function Factorization_Prime_Number([long]$n){
+    if($n -ge 2){
+        
+        [System.String]$str = ""
+        $sqr = [System.Math]::Sqrt($n)
+        while(($n%2) -eq 0 -and 2 -le $sqr){
+            $str += "2 × "
+            [long]$n = $n / 2
+            $sqr = [System.Math]::Sqrt($n)
+        }
+
+        for($i=3; $i -le $sqr; $i += 2){
+            while (($n % $i) -eq 0 -and $i -le $sqr){
+                $str += [string]$i + " × "
+                [long]$n = $n / $i
+                $sqr = [System.Math]::Sqrt($n)
+            }
+        }
+        if ($str-ne ""){
+            $str += [string]$n
+        }
+        return $str
+    }else{
+    return "2以上の整数を入力してください。"
+    }
+}
+
+
+[int16]$flg = 6
+[int16]$j = 1
+while($flg -eq 6 -and $j -le 10){
+    $n = [Interaction]::InputBox("整数を入力してください:")
+    $str = Factorization_Prime_Number $n
+
+    if ($str -eq ""){
+        $flg = [Interaction]::MsgBox("素数です。", 4)
+     }elseif($str -eq "2以上の整数を入力してください。"){
+        $flg = [Interaction]::MsgBox($str, 4)
+     }else{
+        $flg = [Interaction]::MsgBox( "素数ではありません。`r`n" + $str, 4)
+     }
+    $j += 1
+}
